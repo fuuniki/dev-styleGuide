fda652856c0dafbbda19324f18524aa491a34ae7
fuuniki <kaedeyuzu02@gmail.com>
2022-08-07 22:39:22 +0900

    main sidebar

---
+++ src/js/script.js
@@ -0,0 +1,34 @@
+/* slick animation */
+jQuery('.slider').slick({
+  fade:true,
+  centerMode:true,
+  centerPadding:'0px',
+  dots: true,
+  arrows: false,
+  infinite: true,
+  autoplay:true,
+  adaptiveHeight:true,
+  autoplaySpeed:2500,
+  speed: 1000,
+  pauseOnHover: false,
+  // pauseOnDotsHover: false,
+  // pauseOnFocus: false,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  accessibility:true,
+});
+
+/* accodion */
+jQuery(function($) {
+  $('.js-yearHeading').click(function(){
+    if($(this).next('.js-months').is(':visible')) { //既に開いている場所なら
+      $(this).next('.js-months').slideUp(300); //閉じる
+      $(this).children('.js-toggleIcon').removeClass('open'); //.acv_openを削除
+    } else { //閉じている場所なら
+      $('.js-months').slideUp(); //全部閉じる
+      $('.js-monthlyArchive').find('.js-toggleIcon').removeClass('open'); //.acv_open全削除
+      $(this).next('.js-months').slideDown(300); //開く
+      $(this).children('.js-toggleIcon').addClass('open'); //.acv_open付加
+    }
+  });
+});
